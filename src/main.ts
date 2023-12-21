1b4150035b734586851033d63582e38da44f38f1
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-12-21 07:25:31 -0300

    published

--- src/main.ts
+++ src/main.ts
@@ -17,13 +17,13 @@ const encrypted = sjcl.encrypt(secretKey, JSON.stringify(payload));
 
 server.get('/', async (request: Request, response: Response) => {
     //response.cookie('hash', encrypted);
-    /*response.cookie('hash', encrypted, {
+    response.cookie('hash', encrypted, {
         domain: 'app-olga-2.vercel.app',
         path: '/wiipo',
         secure: true, 
         sameSite: 'none',
-      });*/
-    response.setHeader('X-Hash', JSON.stringify(encrypted));
+    });
+    //response.setHeader('X-Hash', JSON.stringify(encrypted));
     return response.redirect(302, 'https://app-olga-2.vercel.app/wiipo')
 });
 
